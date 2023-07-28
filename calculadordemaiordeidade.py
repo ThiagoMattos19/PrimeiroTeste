f2006e1bff8af0ee85d2e024ec53361313740284
Thiago Mattos <thiagomattos.07@hotmail.com>
2023-07-28 00:33:10 -0300

    primeiro teste do código

---
+++ calculadordemaiordeidade.py
@@ -0,0 +1,21 @@
+import os
+print('-'*20)
+idade = int(input('Digite a sua idade: '))
+print('-'*20)
+
+if idade  >= 18:
+    print('-'*20)
+    print('você é maior de idade')
+    print('-'*20)
+else:
+    print('-'*20)
+    print('Você é menor de idade')
+    print('-'*20)
+
+
+    os.system("pause")
+
+
+
+    
+    
